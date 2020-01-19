c4b0acdcc2252fedf238f55c44decb05ebcd7b40
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-19 20:14:44 +0300

    injectated router service to the create event component and used nagivate to navigate to all events

--- src/app/events/create-event.component.ts
+++ src/app/events/create-event.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   template: `
@@ -9,8 +10,14 @@ import { Component } from '@angular/core';
         <br/>
         <br/>
         <button type="submit" class="btn btn-primary">Save</button>
+        <button type="button" class="btn btn-default" (click)="cancel()">Cancel</button>
     </div>
   `
 })
 export class CreateEventComponent {
+  constructor(private router: Router ) {
+  }
+  cancel() {
+    this.router.navigate(['/events']).then(r => console.log(r)).catch(e => console.log(e));
+  }
 }
