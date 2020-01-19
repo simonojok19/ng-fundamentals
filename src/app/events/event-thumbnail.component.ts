fcee1b28c9c4908f1bcf6e4d46bc1b36e84986a3
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-19 11:52:29 +0300

    added css to controll the thumbnail

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -15,6 +15,7 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
   </div>
   `,
   styles: [`
+    .thumbnail { min-height: 210px }
     .pad-left { margin-left: 10px }
     .well div { color: #bbb }
   `]
