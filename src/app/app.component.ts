ce22c43a8005e12477810a1ba0fcc0754645de12
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-19 11:08:59 +0300

    added navbar component to the main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-      <app-events-list></app-events-list>
+    <app-navbar></app-navbar>
+    <app-events-list></app-events-list>
   `
 })
 export class AppComponent {
