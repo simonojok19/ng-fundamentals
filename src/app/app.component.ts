21b8c04ba8f767ed9f23cfe00c91879da0fd79c5
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-19 09:09:50 +0300

    finished the first modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>Hello world</h1>'
+  template: `
+      <h1>Hello world</h1>
+      <img src="/assets/images/basic-shield.png"/>
+  `
 })
 export class AppComponent {
   title = 'ng-fundamentals';
