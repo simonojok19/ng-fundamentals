f3ea2ee78fce0a6a66f5f4ba264b8352c49626a2
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-19 10:09:34 +0300

    added databinded componenent 33:32

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -4,6 +4,18 @@ import { Component } from '@angular/core';
   selector: 'app-events-list',
   template: `<div>
       <h1>Upcoming Angular Events</h1>
+      <hr>
+      <div class="well hoverwell thumbnail">
+          <h2>{{ event.name }}</h2>
+          <div>Date: {{ event.date }}</div>
+          <div>Time: {{ event.time }}</div>
+          <div>Price: \${{ event.price }}</div>
+          <div>
+              <span>Location: {{ event.location.address }}</span>
+              <span>&nbsp;</span>
+              <span>{{ event.location.city }}, {{ event.location.country }}</span>
+          </div>
+      </div>
   </div>`
 })
 export class EventsListComponent {
@@ -15,11 +27,9 @@ export class EventsListComponent {
     price: 599.99,
     imageUrl: '/assets/images/angularconnect-shield.png',
     location: {
-      address: {
-        address: '1057 DT',
-        city: 'London',
-        country: 'England'
-      }
+      address: '1057 DT',
+      city: 'London',
+      country: 'England'
     }
   };
 }
