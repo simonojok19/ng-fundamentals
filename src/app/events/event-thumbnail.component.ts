0b3d24f39db25cc1ad273698c2a7d83dd2df6e24
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-19 12:33:21 +0300

    used class binding with ngClass

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,7 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
     <div class="well hoverwell thumbnail">
     <h2>{{ event?.name }}</h2>
     <div>Date: {{ event?.date }}</div>
-    <div [class.green]="event?.time === '8:00 am'" [ngSwitch]="event?.time">Time: {{ event?.time }}
+    <div [ngClass] ="{green: event?.time === '8:00 am', bold: event?.time === '8:00 am'}" [ngSwitch]="event?.time">Time: {{ event?.time }}
         <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
         <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
         <span *ngSwitchDefault>(Normal Start)</span>
@@ -23,6 +23,7 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
   `,
   styles: [`
     .green { color: #003300 !important;}
+    .bold { font-weight: bold; }
     .thumbnail { min-height: 210px }
     .pad-left { margin-left: 10px }
     .well div { color: #bbb }
