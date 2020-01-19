a033c244ac19a187c778dd071814a37c03676f2b
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-19 21:34:30 +0300

    implemented EventRouteActivator

--- src/app/events/event-details/event-route-activator.service.ts
+++ src/app/events/event-details/event-route-activator.service.ts
@@ -1,6 +1,16 @@
 import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {EventService} from '../shared/event.service';
 
 @Injectable()
-export class EventRouteActivatorService {
-  constructor() {}
+export class EventRouteActivator implements CanActivate {
+  constructor(private eventService: EventService, private router: Router) {}
+  canActivate(route: ActivatedRouteSnapshot) {
+    const eventExists = !!this.eventService.getEvent(route.params.id);
+    if (!eventExists) {
+      this.router.navigate(['/404']).then(r => console.log(r)).catch(e => console.log(e));
+    }
+    return true;
+  }
 }
