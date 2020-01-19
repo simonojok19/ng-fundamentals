490fd4db1c57c03be18e4ed4a849881efdec8da4
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-19 11:06:33 +0300

    copied navbar container template from helper folder

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar'
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html'
 })
 export class NavBarComponent {
 
