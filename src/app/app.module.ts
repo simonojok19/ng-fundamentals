d80aa918b13c33c47f29ba3d32c69405528e6305
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-19 10:24:53 +0300

    added event thumbnail that recieves inputs from events-list componet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {EventsListComponent} from './events/events-list.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventsListComponent
+    EventsListComponent,
+    EventThumbnailComponent
   ],
   imports: [
     BrowserModule
