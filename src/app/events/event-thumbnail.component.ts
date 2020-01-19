d596a9d13598bbc90631a844d2994934cd639d55
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-19 12:47:34 +0300

    refactored ngClass to use function

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,7 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
     <div class="well hoverwell thumbnail">
     <h2>{{ event?.name }}</h2>
     <div>Date: {{ event?.date }}</div>
-    <div [ngClass] ="{green: event?.time === '8:00 am', bold: event?.time === '8:00 am'}" [ngSwitch]="event?.time">Time: {{ event?.time }}
+    <div [ngClass] ="getStartTimeClass()" [ngSwitch]="event?.time">Time: {{ event?.time }}
         <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
         <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
         <span *ngSwitchDefault>(Normal Start)</span>
@@ -31,4 +31,9 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 })
 export class EventThumbnailComponent {
   @Input() event: any;
+
+  getStartTimeClass() {
+    const isEarlyStart =  this.event && this.event.time === '8:00 am';
+    return { green: isEarlyStart, bold: isEarlyStart }
+  }
 }
