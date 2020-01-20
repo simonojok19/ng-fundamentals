5243392947a4d657b1c714e945d30acc93d136f9
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-20 09:28:54 +0300

    added routes and registered profile component

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import {ProfileComponent} from './profile.component';
 
 @NgModule({
   imports: [
+    CommonModule,
+    RouterModule.forRoot(userRoutes)
   ],
   declarations: [
+    ProfileComponent
   ],
   providers: [
   ]
