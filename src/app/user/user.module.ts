74116c250498ce6924c8b0663d69758001a35847
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-20 09:39:16 +0300

    fixed some error forRoot was used trice

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -7,7 +7,7 @@ import {NgModule} from '@angular/core';
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forRoot(userRoutes)
+    RouterModule.forChild(userRoutes)
   ],
   declarations: [
     ProfileComponent
