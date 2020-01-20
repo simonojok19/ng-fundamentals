a443d4232f680787b0bf762ed0b9481f51b11bb6
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-20 09:05:51 +0300

    added a UserModule

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [
+  ],
+  providers: [
+  ]
+})
+export class UserModule {
+}
