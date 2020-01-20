449becd78d7453c4bbcdba859f5f7df8214b9e4f
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-20 08:20:22 +0300

    resolver finished

--- src/app/events/events-list-resolver.service.ts
+++ src/app/events/events-list-resolver.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Resolve } from '@angular/router';
+import {EventService} from './shared/event.service';
+import {map} from 'rxjs/operators';
 
 @Injectable()
-export class EventListResolver implements Resolve<any>{
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> | Promise<any> | any {
-    return undefined;
+export class EventListResolver implements Resolve<any> {
+  constructor(private eventService: EventService) { }
+  resolve() {
+    return this.eventService.getEvents().pipe(map(events => events));
   }
 
 }
