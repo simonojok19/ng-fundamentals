00ee67c9d3e06f5387462b70ae3e19bd0976fb66
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-20 10:57:13 +0300

    added forms module to user module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,11 +4,13 @@ import {ProfileComponent} from './profile.component';
 import {userRoutes} from './user.routes';
 import {NgModule} from '@angular/core';
 import {LoginComponent} from './login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(userRoutes)
+    RouterModule.forChild(userRoutes),
+    FormsModule
   ],
   declarations: [
     ProfileComponent,
