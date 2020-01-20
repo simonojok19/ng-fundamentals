4f6cce9982ac87be93c7d3b789705532d7697a4d
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-20 11:33:22 +0300

    implemented the login method in the Login componet to send data using auth service

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AuthService} from './auth.service';
 
 @Component({
   templateUrl: './login.component.html'
@@ -6,8 +7,9 @@ import { Component } from '@angular/core';
 export class LoginComponent {
   userName;
   password;
+  constructor(private authService: AuthService) {}
 
   login(formValues: any) {
-    console.log(formValues);
+    this.authService.loginUser(formValues.userName, formValues.password);
   }
 }
