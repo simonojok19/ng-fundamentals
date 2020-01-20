c09d1791e88a6d48fddcbd213f8e24ab59bdecc9
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-20 11:26:20 +0300

    implemented dummy loginUser methode, later it will be used for login in user to the server

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import {IUser} from './user.model';
 
 @Injectable()
 export class AuthService {
   currentUser: IUser;
   loginUser(userName: string, password: string) {
-
+    this.currentUser = {
+      id: 1,
+      userName,
+      firstName: 'Simon',
+      lastName: 'Peter'
+    };
   }
 }
