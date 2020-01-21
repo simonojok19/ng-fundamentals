3d90ea90e1ffa3a408f7e436ad58eb154076e244
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-21 09:39:18 +0300

    Validating, added css for styling the inputes dur to errors

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -4,7 +4,15 @@ import {AuthService} from './auth.service';
 import {Router} from '@angular/router';
 
 @Component({
- templateUrl: './profile.component.html'
+ templateUrl: './profile.component.html',
+  styles: [`
+    em { float: right; color: #e05c65; padding-right: 10px; }
+    .error input { background-color: #e3c3c5 }
+    .error ::-webkit-input-placeholder { color: #999 }
+    .error ::-moz-placeholder { color: #999 }
+    .error :-moz-placeholder { color: #999 }
+    .error :-ms-input-placeholder { color: #999 }
+  `]
 })
 export class ProfileComponent implements OnInit{
   public firstName: FormControl;
