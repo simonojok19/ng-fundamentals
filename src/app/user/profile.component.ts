b21b721860c584b9d9a59e91c41fcf75d3f98d77
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-21 09:58:07 +0300

    Using, added pattern validator

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -20,7 +20,10 @@ export class ProfileComponent implements OnInit{
   public profileForm: FormGroup;
   constructor(private authService: AuthService, private router: Router) {}
   ngOnInit(): void {
-    this.firstName = new FormControl(this.authService.currentUser.firstName, Validators.required);
+    this.firstName = new FormControl(this.authService.currentUser.firstName, [
+      Validators.required,
+      Validators.pattern('[a-zA-Z].*')
+    ]);
     this.lastName = new FormControl(this.authService.currentUser.lastName, Validators.required);
     this.profileForm = new FormGroup({
       firstName: this.firstName,
