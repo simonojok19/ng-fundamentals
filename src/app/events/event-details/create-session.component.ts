320d88567cc696321c87dcd4eb6cd11d903fc62f
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-21 11:49:59 +0300

    done some mapping in the saveSession methon on create seesion componenet

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {ISession} from '../shared/event.model';
 
 @Component({
   templateUrl: './create-session.component.html'
@@ -27,6 +28,15 @@ export class CreateSessionComponent implements OnInit{
   }
 
   saveSession(formValues: any) {
-    console.log(formValues);
+    const session: ISession = {
+      id: undefined,
+      name: formValues.name,
+      duration: +formValues.duration,
+      level: formValues.level,
+      abstract: formValues.abstract,
+      presenter: formValues.presenter,
+      voters: []
+    };
+    console.log(session);
   }
 }
