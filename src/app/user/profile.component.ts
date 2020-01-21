6b9be8306b59a87868a5f6ddcefb525903f0ded3
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-21 09:13:02 +0300

    Creating, added nagivation to the events component

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import {AuthService} from './auth.service';
+import {Router} from '@angular/router';
 
 @Component({
  templateUrl: './profile.component.html'
@@ -9,7 +10,7 @@ export class ProfileComponent implements OnInit{
   public firstName: FormControl;
   public lastName: FormControl;
   public profileForm: FormGroup;
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
   ngOnInit(): void {
     this.firstName = new FormControl(this.authService.currentUser.firstName);
     this.lastName = new FormControl(this.authService.currentUser.lastName);
@@ -19,4 +20,7 @@ export class ProfileComponent implements OnInit{
     });
   }
 
+  cancel() {
+    this.router.navigate(['events']).then(r => console.log(r)).catch(e => console.log(e));
+  }
 }
