3e76f3969e0ac3210f0b935ccccad28f434965ba
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-21 11:42:53 +0300

    added level form control which was missing

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -10,15 +10,18 @@ export class CreateSessionComponent implements OnInit{
     duration: FormControl;
     abstract: FormControl;
     newSessionForm: FormGroup;
+    level: FormControl;
   ngOnInit(): void {
     this.name = new FormControl('', Validators.required);
     this.presenter = new FormControl('', Validators.required);
     this.duration = new FormControl('', Validators.required);
+    this.level = new FormControl('', Validators.required);
     this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)]);
     this.newSessionForm = new FormGroup({
       name: this.name,
       presenter: this.presenter,
       duration: this.duration,
+      level: this.level,
       abstract: this.abstract
     });
   }
