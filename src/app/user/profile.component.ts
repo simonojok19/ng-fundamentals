c6ccb6223c5b07e1a1ff03c5d6a18ac0c5e813e4
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-21 08:39:14 +0300

    Creating, passed in the default values to be used by the form controls as the first arguments

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
+import {AuthService} from './auth.service';
 
 @Component({
  templateUrl: './profile.component.html'
@@ -8,9 +9,10 @@ export class ProfileComponent implements OnInit{
   public firstName: FormControl;
   public lastName: FormControl;
   public profileForm: FormGroup;
+  constructor(private authService: AuthService) {}
   ngOnInit(): void {
-    this.firstName = new FormControl();
-    this.lastName = new FormControl();
+    this.firstName = new FormControl(this.authService.currentUser.firstName);
+    this.lastName = new FormControl(this.authService.currentUser.lastName);
     this.profileForm = new FormGroup({
       firstName: this.firstName,
       lastName: this.lastName
