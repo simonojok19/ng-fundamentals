168d3cf2bb39aef628096c91ba77fd833507ae6f
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-21 11:23:45 +0300

    Instantiated all the form controls in the ng on init

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   templateUrl: './create-session.component.html'
 })
-export class CreateSessionComponent {
+export class CreateSessionComponent implements OnInit{
+    name: FormControl;
+  private presenter: FormControl;
+  private duration: FormControl;
+  private abstract: FormControl;
+  ngOnInit(): void {
+    this.name = new FormControl('', Validators.required);
+    this.presenter = new FormControl('', Validators.required);
+    this.duration = new FormControl('', Validators.required);
+    this.abstract = new FormControl('', Validators.required);
+  }
 
 }
