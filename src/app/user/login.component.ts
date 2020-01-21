6e480bba9093c0ada42f9b6746c7e8a2b33654fa
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-21 08:14:41 +0300

    Validations added styling for the em tags

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -3,7 +3,10 @@ import {AuthService} from './auth.service';
 import {Route, Router} from '@angular/router';
 
 @Component({
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styles: [`
+    em { float: right; color: #e05c65; padding-left: 10px}
+  `]
 })
 export class LoginComponent {
   userName;
