cb91b03718919246608c4b2a9297b0398f263a4e
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-21 08:29:21 +0300

    Creating, added two form control instances in profile componnt

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core'
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
  templateUrl: './profile.component.html'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
+  private firstName: FormControl;
+  private lastName: FormControl;
+  ngOnInit(): void {
+    this.firstName = new FormControl();
+    this.lastName = new FormControl();
+  }
 
 }
