d5f34250963d781bcdb6039ed90b2d36ea19b55f
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-22 10:47:59 +0300

    Cleaned up toastr service from event list component

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { EventService } from './shared/event.service';
-import {ToastrService} from '../common/toastr.service';
+import {TOASTR_TOKEN} from '../common/toastr.service';
 import {ActivatedRoute} from '@angular/router';
 import {IEvent} from './shared/event.model';
 
@@ -13,7 +13,7 @@ import {IEvent} from './shared/event.model';
           <hr>
           <div class="row">
               <div class="col-md-5" *ngFor="let event of events">
-                  <app-event-thumbnail [event]="event" (click)="handleThumbnailClick(event.name)"></app-event-thumbnail>
+                  <app-event-thumbnail [event]="event"></app-event-thumbnail>
               </div>
           </div>
       </div>
@@ -23,17 +23,12 @@ export class EventsListComponent implements OnInit{
   events: IEvent[];
   constructor(
     private eventService: EventService,
-    private toastrService: ToastrService,
     private activatedRoute: ActivatedRoute) {
   }
   ngOnInit() {
     this.eventService.getEvents().subscribe( (event: any) => {
       this.events = event;
     });
-  }
-
-  handleThumbnailClick(eventName: any) {
-    this.toastrService.success(eventName);
   }
 }
 
