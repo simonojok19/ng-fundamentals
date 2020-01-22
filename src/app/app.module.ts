56cffc81c16e8a8f055149e80288c411768d9c64
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-22 08:03:58 +0300

    declared the component in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import {AuthService} from './user/auth.service';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {CreateSessionComponent} from './events/event-details/create-session.component';
 import {SessionListComponent} from './events/event-details/session-list.component';
+import {CollapsibleWellComponent} from './common/collapsible-well.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import {SessionListComponent} from './events/event-details/session-list.componen
     CreateEventComponent,
     Error404Component,
     CreateSessionComponent,
-    SessionListComponent
+    SessionListComponent,
+    CollapsibleWellComponent,
   ],
   imports: [
     BrowserModule,
