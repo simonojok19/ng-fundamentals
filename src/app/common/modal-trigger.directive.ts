071c95e386280166b3c2b7da51279abcfb7404e8
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-22 12:43:51 +0300

    Using ElementRef to get access html element in which the directive is found

--- src/app/common/modal-trigger.directive.ts
+++ src/app/common/modal-trigger.directive.ts
@@ -1,11 +1,14 @@
-import {Directive, Inject, OnInit} from '@angular/core';
+import {Directive, ElementRef, Inject, OnInit} from '@angular/core';
 import {JQ_TOKEN} from './jquery.service';
 
 @Directive({
   selector: '[appModalTrigger]'
 })
 export class ModalTriggerDirective implements OnInit {
-  constructor(@Inject(JQ_TOKEN) private $: any) {}
+  private htmlElement: HTMLElement;
+  constructor(elementRef: ElementRef, @Inject(JQ_TOKEN) private $: any) {
+    this.htmlElement = elementRef.nativeElement;
+  }
   ngOnInit(): void {
     this.$('#simple-modal').modal({});
   }
