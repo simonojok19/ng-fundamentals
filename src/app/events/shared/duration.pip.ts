0f14df4999c33fa6ce3652346b7f6c923173eade
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-22 09:19:48 +0300

    implemented the duration pipe

--- src/app/events/shared/duration.pip.ts
+++ src/app/events/shared/duration.pip.ts
@@ -4,5 +4,14 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'duration'
 })
 export class DurationPip implements PipeTransform {
+  transform(value: number): string {
+    switch (value) {
+      case 1: return 'Half Hour';
+      case 2: return 'One Hour';
+      case 3: return 'Half Day';
+      case 4: return 'Full Day';
+      default: return value.toString();
+    }
+  }
 
 }
