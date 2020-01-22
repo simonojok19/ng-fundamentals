0b1b7f6d587d1f960759f18c93ab406d4a99782f
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-22 09:56:57 +0300

    Creating a private method filterSession

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,11 +1,26 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnChanges} from '@angular/core';
 import {ISession} from '../shared/event.model';
 
 @Component({
   selector: 'app-session-list',
   templateUrl: './session-list.component.html'
 })
-export class SessionListComponent {
+export class SessionListComponent implements OnChanges{
   @Input() sessions: ISession;
   @Input() filterBy: string;
+  visibleSessions: ISession;
+
+  ngOnChanges(): void {
+    if (this.sessions) {
+      this.filterSession(this.filterBy);
+    }
+  }
+
+  private filterSession(filter: string) {
+    if (filter === 'all') {
+
+    } else {
+
+    }
+  }
 }
