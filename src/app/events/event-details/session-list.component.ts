cfba40ef476e9d89e1ed9f8f333ba1578c1557c5
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-22 10:18:36 +0300

    Added and implemented sortByNameAsc stateless method

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -8,11 +8,15 @@ import {ISession} from '../shared/event.model';
 export class SessionListComponent implements OnChanges{
   @Input() sessions: ISession[];
   @Input() filterBy: string;
+  @Input() sortBy: string;
   visibleSessions: ISession[];
 
   ngOnChanges(): void {
     if (this.sessions) {
       this.filterSession(this.filterBy);
+      this.sortBy === 'names' ?
+        this.visibleSessions.sort(sortByNameAsc) :
+        this.visibleSessions.sort(sortByVotesDesc);
     }
   }
 
@@ -25,4 +29,8 @@ export class SessionListComponent implements OnChanges{
       });
     }
   }
+}
+
+function sortByNameAsc(s1: ISession, s2: ISession) {
+  if (s1.name > s2.name) { return 1; } else if (s1.name === s2.name) { return 0; } else { return -1; }
 }
