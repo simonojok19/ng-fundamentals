8bff76bae616ac8d4687edfd0b9391ada9978ce7
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-22 10:40:47 +0300

    Used long hand approach to register toastr token to the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
 import { EventService } from './events/shared/event.service';
-import { ToastrService } from './common/toastr.service';
+import { TOASTR_TOKEN } from './common/toastr.service';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { RouterModule } from '@angular/router';
 import {appRoutes} from './routes';
@@ -21,6 +21,7 @@ import {SessionListComponent} from './events/event-details/session-list.componen
 import {CollapsibleWellComponent} from './common/collapsible-well.component';
 import {DurationPipe} from './events/shared/duration.pipe';
 
+declare let toastr: any;
 @NgModule({
   declarations: [
     AppComponent,
@@ -43,7 +44,7 @@ import {DurationPipe} from './events/shared/duration.pipe';
   ],
   providers: [
     EventService,
-    ToastrService,
+    { provide: TOASTR_TOKEN, useValue: toastr },
     EventRouteActivator,
     { provide: 'canDeactivateCreateEvent', useValue: checkDirtyState },
     EventListResolver,
---
+++ src/app/common/toastr.service.ts
@@ -0,0 +1,4 @@
+import { InjectionToken } from '@angular/core';
+
+
+export let TOASTR_TOKEN = new InjectionToken<>('toastr');
