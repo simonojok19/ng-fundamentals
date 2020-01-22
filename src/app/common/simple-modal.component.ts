440fbc945df77c9c8e3e072df7e75e939b92366b
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-22 12:03:38 +0300

    Implemented app-simple-modal component

---
+++ src/app/common/simple-modal.component.ts
@@ -0,0 +1,28 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-simple-model',
+  template: `
+    <div id="simple-modal" class="modal fade" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="model-header">
+                    <button type="button" class="close" data-dismiss="modal">
+                        <span>&times;</span>
+                        <h4 class="modal-title">{{ title }}</h4>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <ng-content></ng-content>
+                </div>
+            </div>
+        </div>
+    </div>
+  `,
+  styles: [`
+    .modal-body { height: 250px; overflow-y: scroll; }
+  `]
+})
+export class SimpleModalComponent {
+  @Input() title: string;
+}
