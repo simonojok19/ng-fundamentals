488e9ccbc8e8b03f50d2dbcc62978e78f09706c1
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-22 13:21:44 +0300

    Routing to the Same Component => fixed some bug

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {EventService} from '../shared/event.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Params} from '@angular/router';
 import {IEvent, ISession} from '../shared/event.model';
 
 @Component({
@@ -19,7 +19,10 @@ export class EventDetailsComponent implements OnInit {
   constructor(private eventService: EventService, private activatedRoute: ActivatedRoute) {
   }
   ngOnInit(): void {
-    this.event = this.eventService.getEvent(+this.activatedRoute.snapshot.params.id);
+    this.activatedRoute.params.forEach((params: Params) => {
+      this.event = this.eventService.getEvent(+params.id);
+    });
+    // this.event = this.eventService.getEvent(+this.activatedRoute.snapshot.params.id);
   }
 
   addSession() {
